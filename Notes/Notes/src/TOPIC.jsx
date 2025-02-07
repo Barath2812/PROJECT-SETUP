212ad985b9d46d16e6afe41bf65fff33a4965323
Barath2812 <yaaroivan64@gmail.com>
2025-02-07 20:09:31 +0530

    Update TOPIC.jsx

--- Notes/Notes/src/TOPIC.jsx
+++ Notes/Notes/src/TOPIC.jsx
@@ -38,14 +38,14 @@ const TopicPage = ({ userRole }) => {
         <nav className="navbar navbar-dark navbar-expand-lg">
           <div className="container">
             <h1 className="navbar-brand">SATHYABAMA</h1>
-            <button
+            <button4
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNav"
             >
               <span className="navbar-toggler-icon"></span>
-            </button>
+            </button4>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
